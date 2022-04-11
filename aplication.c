524777417a715fccfb8bbceae1a5db91e381eabe
SSanti32 <slarroude@itba.edu.ar>
2022-04-11 11:02:00 -0300

    Co-authored-by: NeksHinto <nhinojo@itba.edu.ar> Co-authored-by: Mateo Menghini <matthewm00@users.noreply.github.com>

---
+++ aplication.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (int argc, char const *argv[]){
+
+/*
+- funcion que valide el formato de archivo que nos pasan
+- crear los pipes para que se comuniquen los workers con el aplication (lo podemos hacer)
+- crear los workes (lo podemos hacer)
+- 
+*/
+
+}
